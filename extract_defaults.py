8bb7d1a337c57989b79731f8404a1b52ea7781c2
Trevor Hubbard <tmhubbard@umass.edu>
2022-02-17 21:46:11 -0500

    Git cleanup

--- extract_defaults.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-
-params_df = pd.read_excel("Parameter Explanations.xlsx")
-important_params_df = params_df.query("type=='str' | type=='int' | type=='bool' | type=='float'")
-param_defaults_dict = {row["parameter_name"]:row["default_colab_value"] for idx, row in important_params_df.iterrows()}
-with open("param_defaults.json", "w") as param_json:
-	json.dump(param_defaults_dict, param_json, indent=4)
